fce45856b13d8fb01742130ff45e6620463d9c20
SohaibMushtaq621 <mohsintariq1335@gmail.com>
2022-02-09 15:04:01 +0500

    notification added

--- src/Navigation/Bottom/index.js
+++ src/Navigation/Bottom/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MemberArea } from '../../Screens'
+import {
+    MaterialCommunityIcons,
+    Octicons,
+    Foundation,
+    Ionicons
+} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,10 +17,46 @@ export default function App() {
                 headerShown: false
             }}
         >
-            <Tab.Screen name="OverView" component={MemberArea.BottomBar.OverView} />
-            <Tab.Screen name="ProjectUpdates" component={MemberArea.BottomBar.ProjectUpdates} />
-            <Tab.Screen name="MyUnitDetails" component={MemberArea.BottomBar.MyUnitDetails} />
-            <Tab.Screen name="Inquiries" component={MemberArea.BottomBar.Inquiries} />
+            <Tab.Screen
+                options={{
+                    tabBarActiveTintColor: 'blue',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="view-quilt-outline" size={30} color={color} />
+                    )
+                }}
+                name="OverView"
+                component={MemberArea.BottomBar.OverView} />
+            <Tab.Screen
+                options={{
+                    tabBarActiveTintColor: 'blue',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarIcon: ({ color }) => (
+                        <Octicons name="project" size={24} color={color} />
+                    )
+                }}
+                name="ProjectUpdates"
+                component={MemberArea.BottomBar.ProjectUpdates} />
+            <Tab.Screen
+                options={{
+                    tabBarActiveTintColor: 'blue',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarIcon: ({ color }) => (
+                        <Foundation name="clipboard-pencil" size={24} color={color} />
+                    )
+                }}
+                name="MyUnitDetails"
+                component={MemberArea.BottomBar.MyUnitDetails} />
+            <Tab.Screen
+                options={{
+                    tabBarActiveTintColor: 'blue',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarIcon: ({color}) => (
+                        <Ionicons name="chatbubbles-outline" size={24} color={color} />
+                    )
+                }}
+                name="Inquiries"
+                component={MemberArea.BottomBar.Inquiries} />
         </Tab.Navigator>
     );
 }
--- src/Navigation/Stack/MemberArea.js
+++ src/Navigation/Stack/MemberArea.js
@@ -2,13 +2,72 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomBarNavigation from '../Bottom';
 import { MemberArea } from '../../Screens'
+import { TouchableOpacity, View, Image, Dimensions, StyleSheet, Pressable } from 'react-native';
+import { Avatar, Badge, Icon, withBadge } from 'react-native-elements';
+import { Feather } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+
 const Stack = createNativeStackNavigator();
+
+const style = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+});
 
 function MemberAreaStack() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="MemberArea" component={BottomBarNavigation} />
-            <Stack.Screen name="Notification" component={MemberArea.Main.Notification} />
+            <Stack.Screen
+                options={({ navigation }) =>({
+                    title: '',
+                    headerLeft: () => (
+                        <TouchableOpacity>
+                            <Image
+                                //   style={{
+                                //       height:30
+                                //   }}
+                                source={require('../../asserts/BottomsIcons/Group.png')}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerRight: () => {
+                        return (
+                            <View style={style.container}>
+                                <Pressable
+                                    android_ripple={{ color: '#c4c4c4', borderless: true, radius: 30 }}
+                                    onPress={()=>navigation.navigate("Notification")}
+                                    style={{ marginRight: 30 }}
+                                >
+
+                                    <Feather name="bell" size={24} color="black" />
+                                    <Badge
+                                        status="error"
+                                        value={10}
+                                        containerStyle={{ position: 'absolute', bottom: 15, left: 20 }}
+                                    />
+                                </Pressable>
+                                <TouchableOpacity>
+                                    <Avatar
+                                        rounded
+                                        source={require('../../asserts/BottomsIcons/placeholder.png')}
+                                        size="small"
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                        );
+                    }
+                })}
+                name="MemberArea"
+                component={BottomBarNavigation} />
+            <Stack.Screen 
+            options={{
+                title:'back'
+            }}
+            name="Notification" 
+            component={MemberArea.Main.Notification} />
         </Stack.Navigator>
     );
 }

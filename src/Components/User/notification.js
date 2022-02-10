d0742b0f5f707cda26fe6fb168caaba348aba498
SohaibMushtaq621 <mohsintariq1335@gmail.com>
2022-02-10 17:12:20 +0500

    small changes

--- src/Components/User/notification.js
+++ src/Components/User/notification.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        alignItems: 'center',
+        paddingRight:wp(20)
     },
     date: {
         color: '#075595',
